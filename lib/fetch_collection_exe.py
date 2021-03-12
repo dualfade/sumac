2c86ac6ff7a5af188203243127ecd7ff8dcfc416
dualfade <dualfade@vadersecurity.com>
2021-03-11 20:33:45 -0500

    + file stat

--- lib/fetch_collection_exe.py
+++ lib/fetch_collection_exe.py
@@ -43,3 +43,7 @@ def stat_file(w_file):
     print(file_stats)
     print(f'File Size in Bytes is {file_stats.st_size}')
     print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+
+
+
+# __EOF__
